5205a9086534fb96a4b3512af23df641d6d15f31
Pratik18Rathore <rathore18pratik@gmail.com>
2025-02-11 17:09:13 +0530

    Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -42,6 +42,10 @@ const Footer = () => {
           >
             <img src={Map} alt="Map" />
           </a>
+          
+      </div>
+      <div>
+      <span>© 2025 Black Panther Gym. All rights reserved.</span>
       </div>
       <div className="blur blur-f-1"></div>
       <div className="blur blur-f-2"></div>
